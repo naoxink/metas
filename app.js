8799556a231fb573f8779a1e9a2146796e136794
J. Gabriel Corpas <naoxink@icloud.com>
2023-02-15 14:17:25 +0000

    dev

--- app.js
+++ app.js
@@ -38,13 +38,14 @@
     for (region in metasObj) {
       const events = metasObj[region]
       let offset = 0
-      // console.log(`\n\n ${color(region, 'Bright')}`)
-      // console.log('═'.repeat(totalWidth))
+
       const html = []
       events.forEach(meta => {
         // console.log(`\n ${color(meta.name, 'Bright')}`)
         let current = meta.segments[0]
         let next = meta.segments[1]
+        // Limpiar vacíos
+        meta.segments = meta.segments.filter(s => !!s.name)
         meta.segments.forEach(function (phase, phaseIndex) {
           let correctedTime = "" + (startHour + (offset > 59 ? 1 : 0))
           const hour = ("00" + correctedTime).slice(-2)
