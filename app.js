3dce048c9a9a668641143e5f5e56ff1996a32ebb
J. Gabriel Corpas <naoxink@icloud.com>
2023-02-15 14:27:22 +0000

    dev

--- app.js
+++ app.js
@@ -43,6 +43,7 @@
       events.forEach(meta => {
         // Limpiar vacíos
         meta.segments = meta.segments.filter(s => !s || !s.name)
+        console.log(segments)
         let current = meta.segments[0]
         let next = meta.segments[1]
         meta.segments.forEach(function (phase, phaseIndex) {
