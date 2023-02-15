16019e6d36267810f262e07331620a1bc9a129df
J. Gabriel Corpas <naoxink@icloud.com>
2023-02-15 11:29:54 +0000

    test dev

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
-fetch('https://raw.githubusercontent.com/DanielScholte/GuildWars2Companion/master/assets/data/event_timers/meta_events.json').then(console.log)
+(async () => {
+
+  const getMetas = async () => {
+    return await fetch('https://raw.githubusercontent.com/DanielScholte/GuildWars2Companion/master/assets/data/event_timers/meta_events.json')
+  }
+
+  const getMetaHtml = data => `<div class="meta-container">
+    <p class="current"></p>
+    <p class="next"></p>
+  </div>`;
+
+  const getRegionContainer = (title, innerHTML) => `<div class="region>
+    <h3>${title}</h3>
+    ${innerHTML}
+  </div>`
+
+  const metasByRegion = metas => metas.reduce((acc, m) => {
+    if (!acc[m.Region]) {
+      acc[m.Region] = []
+    }
+    acc[m.region].push(m)
+    return acc
+  }, {})
+
+  const notPassed = (hour, minute) => {
+    const currentTime = new Date()
+    return +hour > currentTime.getHours() || (+hour <= currentTime.getHours() && +minute > currentTime.getMinutes())
+  }
+
+  const printMetas = () => {
+    for (region in metasObj) {
+      const regions = metasObj[region]
+      let offset = 0
+      // console.log(`\n\n ${color(region, 'Bright')}`)
+      // console.log('═'.repeat(totalWidth))
+      regions.forEach(meta => {
+        const totalTime = meta.segments.reduce((acc, p) => acc += p.duration, 0)
+        // console.log(`\n ${color(meta.name, 'Bright')}`)
+        let current = null
+        let next = null
+        meta.phases.forEach(function (phase, phaseIndex) {
+          let correctedTime = "" + (startHour + (offset > 59 ? 1 : 0))
+          const hour = ("00" + correctedTime).slice(-2)
+          const minute = ("00" + (offset % 60)).slice(-2)
+          offset += phase.duration;
+          if (phase.name && notPassed(correctedTime, offset % 60)) {
+            console.log(phase.name, correctedTime, offset % 60)
+            // console.log(color(' ██', `Fg${phase.color}`), `${phase.name}: ${hour}:${minute} - ${formatDate(+hour, +minute + +phase.duration)} (${phase.duration}')`)
+          }
+        })
+      })
+    }
+  }
+
+})()
