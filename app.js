1f46db2949e9c6aa79b47058cec87d5ebec93610
J. Gabriel Corpas <naoxink@icloud.com>
2023-02-22 11:26:34 +0000

    time debug

--- app.js
+++ app.js
@@ -62,7 +62,7 @@
 
         let current = map.segments[0]
         let next = null // map.segments[1]
-        map.segments.forEach(function (phase, phaseIndex) {
+        map.segments.reverse().forEach(function (phase, phaseIndex) {
           let correctedTime = "" + (startHour + (offset > 59 ? 1 : 0))
           const hour = ("00" + correctedTime).slice(-2)
           const minute = ("00" + (offset % 60)).slice(-2)
